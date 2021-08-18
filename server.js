37f8174fef8d987f8ff415f0f1501aa9789fa53e
Anurag Sharma <anutheriddle@gmail.com>
2021-08-18 19:11:19 +0530

    use for static middleware and correct linking issue

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const path = require('path')
 
 const app = express()
 
+app.use(express.static('public'))
 
-app.get('/sendMethod',(req,res)=>{
-res.send("Hello From Express")
-})
+// app.get('/sendMethod',(req,res)=>{
+// res.send("Hello From Express")
+// })
 
-app.get('/sendFileMethod',(req,res)=>{
-res.sendFile(path.resolve(__dirname)+`/index.html`)
-})
+// app.get('/sendFileMethod',(req,res)=>{
+// res.sendFile(path.resolve(__dirname)+`/index.html`)
+// })
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname)+`/index.html`)
     })
