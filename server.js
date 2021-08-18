134a95a82bea8e6c9a74a9ae291b1687df2ebb93
Anurag Sharma <anutheriddle@gmail.com>
2021-08-18 18:44:41 +0530

    send file implementation in response

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
-app.get('/',(req,res)=>{
+
+
+app.get('/sendMethod',(req,res)=>{
 res.send("Hello From Express")
+})
+
+app.get('/sendFileMethod',(req,res)=>{
+res.sendFile(path.resolve(__dirname)+`/index.html`)
 })
 const PORT = process.env.PORT || 3000
 app.listen(PORT,()=> console.log(`Server Started at ${PORT}`))
