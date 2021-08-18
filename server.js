3944f69b2c574c165605a56b05cd5821dbcdd2b4
Anurag Sharma <anutheriddle@gmail.com>
2021-08-18 18:59:02 +0530

    added about.html and enable anchor href

--- server.js
+++ server.js
@@ -11,5 +11,11 @@ res.send("Hello From Express")
 app.get('/sendFileMethod',(req,res)=>{
 res.sendFile(path.resolve(__dirname)+`/index.html`)
 })
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname)+`/index.html`)
+    })
+app.get('/about',(req,res)=>{
+    res.sendFile(path.resolve(__dirname)+`/about.html`)
+})
 const PORT = process.env.PORT || 3000
 app.listen(PORT,()=> console.log(`Server Started at ${PORT}`))
