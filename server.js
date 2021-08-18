882471a0f610e20f6eff1fac793160930b3b46f6
Anurag Sharma <anutheriddle@gmail.com>
2021-08-18 19:58:12 +0530

    install ejs and apply changes as per requirement for dynamic content value change

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-
+//telling express we are using ejs using set method which set the value of view engine to ejs
+app.set("view engine", 'ejs')
+//to print the value of view engine
+console.log(app.get('view engine'))
 app.use(express.static('public'))
+// let print view value we will see the path in console we can see it uses path where view file or html present
+console.log(app.get('views'))
+// we can customize the views folder says template folder by dowing this
+//app.set('views',path.resolve(__dirname)+'/templates')
+//console.log(app.get('views'))
 
 // app.get('/sendMethod',(req,res)=>{
 // res.send("Hello From Express")
@@ -13,10 +21,16 @@ app.use(express.static('public'))
 // res.sendFile(path.resolve(__dirname)+`/index.html`)
 // })
 app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname)+`/index.html`)
+    //res.sendFile(path.resolve(__dirname)+`/index.html`)
+    res.render('index',{
+        title:'My Home Page'
+    })
     })
 app.get('/about',(req,res)=>{
-    res.sendFile(path.resolve(__dirname)+`/about.html`)
+    //res.sendFile(path.resolve(__dirname)+`/about.html`)
+    res.render('about', {
+        title: 'About Page'
+    })
 })
 
 app.get('/download',(req,res)=>{
